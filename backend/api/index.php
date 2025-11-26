eb295a66c5bf0fd780d56b5557ecf83e756bbff4
Joao Prrvz <joao.prrvz@eduge.ch>
2025-11-26 22:32:32 +0100

    Update index.php for testing BaseSchema

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,2 +1,13 @@
 <?php
 
+use Openstudy\Schemas\BaseSchema;
+require_once __DIR__."/vendor/autoload.php";
+
+error_reporting(E_ERROR | E_PARSE);
+ini_set('display_errors', 1);
+
+$schema = new BaseSchema([
+	"Name" => 1.3
+]);
+
+var_dump($schema);
