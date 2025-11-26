2b9af28e7bdf70f58cd292ca605378d227108e84
Joao Prrvz <joao.prrvz@eduge.ch>
2025-11-26 22:30:55 +0100

    Add Proprety Attribute

---
+++ backend/api/src/Attributes/Proprety.php
@@ -0,0 +1,12 @@
+<?php
+namespace Openstudy\Attributes;
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class Proprety {
+	public ?string $name;
+	
+	public function __construct(?string $name = null) {
+		$this->name = $name;
+	}
+}
