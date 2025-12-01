7a37a3ce8bfc8d22c2b4d6d613e592aaede52032
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-01 18:58:19 +0100

    Update Login schema propreties to required

--- backend/api/src/Schemas/Login.php
+++ backend/api/src/Schemas/Login.php
@@ -5,7 +5,7 @@ use OpenStudy\Attributes\Validators as VA;
 use OpenStudy\Attributes\Proprety;
 use OpenApi\Attributes as OA;
 
-#[OA\Schema]
+#[OA\Schema(required: ["email", "password"])]
 class Login extends BaseSchema {
 	#[OA\Property, Proprety, VA\Max(50)]
 	public string $email;
