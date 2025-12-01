4d806d65aed65176d7e1953e9ec534f8536f0864
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-01 23:33:06 +0100

    Update routes, update /user/login/ and /user/sign-up to POST

--- backend/api/routes.php
+++ backend/api/routes.php
@@ -1,11 +1,12 @@
 <?php
-use OpenStudy\Controllers AS OC;
+use OpenStudy\Controllers\OpenApiController;
 use OpenStudy\Controllers\UserController;
+
 use Slim\Routing\RouteCollectorProxy;
 
-$app->get("/swagger", [OC\OpenApiController::class, "swagger"]);
+$app->get("/swagger", [OpenApiController::class, "swagger"]);
 
 $app->group("/user" , function (RouteCollectorProxy $group) {
-	$group->get("/login", [UserController::class, "login"]);
-	$group->get("/sign-up", [UserController::class, "signUp"]);
+	$group->post("/login", [UserController::class, "login"]);
+	$group->post("/sign-up", [UserController::class, "signUp"]);
 });
