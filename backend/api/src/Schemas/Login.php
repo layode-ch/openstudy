9eac326643ca1cbb59e73ef32a3c617224f88512
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-01 18:08:10 +0100

    Update Login Schema, Add Schema proprety OpenAPI

--- backend/api/src/Schemas/Login.php
+++ backend/api/src/Schemas/Login.php
@@ -3,10 +3,12 @@ namespace OpenStudy\Schemas;
 
 use OpenStudy\Attributes\Validators as VA;
 use OpenStudy\Attributes\Proprety;
+use OpenApi\Attributes as OA;
 
+#[OA\Schema]
 class Login extends BaseSchema {
-	#[Proprety, VA\Max(50)]
+	#[OA\Property, Proprety, VA\Max(50)]
 	public string $email;
-	#[Proprety, VA\Max(50)]
+	#[OA\Property, Proprety, VA\Max(50)]
 	public string $password;
 }
