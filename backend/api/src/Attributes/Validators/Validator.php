62886b5a1682a80b046cd02bb5dd14d71eff41c0
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-01 23:33:30 +0100

    Update Validator, update return type of validate to ?bool

--- backend/api/src/Attributes/Validators/Validator.php
+++ backend/api/src/Attributes/Validators/Validator.php
@@ -2,5 +2,5 @@
 namespace OpenStudy\Attributes\Validators;
 
 abstract class Validator {
-	public abstract function validate(string $name, mixed $value): bool;
+	public abstract function validate(string $name, mixed $value): ?bool;
 }
