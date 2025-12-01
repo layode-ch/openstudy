ad91949e98d37883dad211a2eed07f0f5dcf20fc
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-01 14:30:57 +0100

    Add UserController

---
+++ backend/api/src/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+namespace OpenStudy\Controllers;
+
+use OpenStudy\Schemas\Login;
+
+class UserController extends BaseController {
+	
+	/**
+	 * 
+	 *
+	 * @param Login $schema
+	 * @return void
+	 */
+	public static function login(Login $schema) {
+
+	}  
+}
