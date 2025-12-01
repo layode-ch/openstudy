482cd04df13baff57bde06f891a402368da26b62
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-01 19:03:15 +0100

    Update routes.php, add /user/login and /user/sign-up

--- backend/api/routes.php
+++ backend/api/routes.php
@@ -1,4 +1,11 @@
 <?php
 use OpenStudy\Controllers AS OC;
+use OpenStudy\Controllers\UserController;
+use Slim\Routing\RouteCollectorProxy;
 
 $app->get("/swagger", [OC\OpenApiController::class, "swagger"]);
+
+$app->group("/user" , function (RouteCollectorProxy $group) {
+	$group->get("/login", [UserController::class, "login"]);
+	$group->get("/sign-up", [UserController::class, "signUp"]);
+});
