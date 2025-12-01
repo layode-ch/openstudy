5eeec814d6fbaee09dec9fe2233b1ec9d4feb99e
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-01 23:34:31 +0100

    Update Login schema, add Filter validator

--- backend/api/src/Schemas/Login.php
+++ backend/api/src/Schemas/Login.php
@@ -7,7 +7,7 @@ use OpenApi\Attributes as OA;
 
 #[OA\Schema(required: ["email", "password"])]
 class Login extends BaseSchema {
-	#[OA\Property, Proprety, VA\Max(50)]
+	#[OA\Property, Proprety, VA\Max(50), VA\Filter(FILTER_VALIDATE_EMAIL)]
 	public string $email;
 	#[OA\Property, Proprety, VA\Max(50)]
 	public string $password;
