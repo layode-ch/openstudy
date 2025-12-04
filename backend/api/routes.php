87f54a947ec44a6252162932b2aca4cb380db78f
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-04 08:44:43 +0100

    Update routes.php , add PUT /set/create

--- backend/api/routes.php
+++ backend/api/routes.php
@@ -1,7 +1,8 @@
 <?php
 use OpenStudy\Controllers\OpenApiController;
+use OpenStudy\Controllers\SetController;
 use OpenStudy\Controllers\UserController;
-
+use OpenStudy\Middlewares\AuthMiddleware;
 use Slim\Routing\RouteCollectorProxy;
 
 $app->get("/swagger", [OpenApiController::class, "swagger"]);
@@ -9,4 +10,8 @@ $app->get("/swagger", [OpenApiController::class, "swagger"]);
 $app->group("/user" , function (RouteCollectorProxy $group) {
 	$group->post("/login", [UserController::class, "login"]);
 	$group->post("/sign-up", [UserController::class, "signUp"]);
+});
+
+$app->group("/set" , function (RouteCollectorProxy $group) {
+	$group->put("/create", [SetController::class, "create"])->add(new AuthMiddleware());
 });
