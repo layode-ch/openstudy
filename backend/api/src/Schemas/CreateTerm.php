c03f85ba48c76d9bd4a0137e538bf3e3712c7625
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-04 09:34:28 +0100

    Add CreateTerm schema

---
+++ backend/api/src/Schemas/CreateTerm.php
@@ -0,0 +1,14 @@
+<?php
+namespace OpenStudy\Schemas;
+
+use OpenApi\Attributes as OA;
+use OpenStudy\Attributes\Validators as VA;
+use OpenStudy\Attributes\Property;
+
+#[OA\Schema]
+class CreateTerm extends BaseSchema {
+	#[OA\Property, Property, VA\Filter(FILTER_SANITIZE_FULL_SPECIAL_CHARS)]
+	public string $original;
+	#[OA\Property, Property, VA\Filter(FILTER_SANITIZE_FULL_SPECIAL_CHARS)]
+	public string $definition;
+}
