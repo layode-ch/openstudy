f8fde34cf3b1118bd6c21752e3b0794797ce2de3
Joao Prrvz <joao.prrvz@eduge.ch>
2025-12-04 09:34:53 +0100

    Add AddTerm schema

---
+++ backend/api/src/Schemas/AddTerms.php
@@ -0,0 +1,26 @@
+<?php
+namespace OpenStudy\Schemas;
+
+use OpenApi\Attributes as OA;
+use OpenStudy\Attributes\Validators as VA;
+use OpenStudy\Attributes\Property;
+use OpenStudy\Models\Term;
+#[OA\Schema]
+class AddTerms extends BaseSchema {
+	/** @var array<Term> */
+	public array $terms;
+
+	#[
+		OA\Property("term", type: "array", 
+			items: new OA\Items(CreateTerm::class
+		)), Property
+	]
+	private array $_terms;
+
+	public function __construct(array $data) {
+		parent::__construct($data);
+		foreach ($this->_terms as $key => $term) {
+			$this->terms[$key] = new CreateTerm($term);
+		}
+	}
+}
